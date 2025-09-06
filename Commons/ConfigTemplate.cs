cc81e8a156e0d2bd75f610c09072e92183971eab
s-ishii-tj <s.ishii@yusoku.co.jp>
2025-09-07 01:33:30 +0900

    Add VehicleSpeedUpgrade

---
+++ Commons/ConfigTemplate.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+namespace AshFox.Subnautica
+{
+    public class ConfigTemplate
+    {
+        private JObject _config;
+
+        public ConfigTemplate(string filePath)
+        {
+            LoadConfig(filePath);
+        }
+
+        public bool LoadConfig(string filePath)
+        {
+            string modDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string fullPath = Path.Combine(modDirectory, filePath);
+            if (!File.Exists(fullPath))
+            {
+                _config = null;
+                return false;
+            }
+            _config = JsonConvert.DeserializeObject<JObject>(File.ReadAllText(fullPath));
+            return true;
+        }
+
+        private JValue getValue(string key)
+        {
+            if (_config == null)
+            {
+                return null;
+            }
+            var parts = key.Split('.');
+            JToken current = _config;
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (current is JObject obj)
+                {
+                    if (!obj.TryGetValue(parts[i], out current))
+                    {
+                        return null;
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            if (current is JValue val)
+            {
+                return val;
+            }
+            return null;
+        }
+
+        public String GetString(string key, string defaultValue = null)
+        {
+            if (_config == null)
+            {
+                return defaultValue;
+            }
+
+            var value = getValue(key);
+            if (value == null || value.Type != JTokenType.String)
+            {
+                return defaultValue;
+            }
+            return value.ToString();
+        }
+
+        public int GetInt(string key, int defaultValue = 0)
+        {
+            if (_config == null)
+            {
+                return defaultValue;
+            }
+            var value = getValue(key);
+            if (value == null || value.Type != JTokenType.Integer)
+            {
+                return defaultValue;
+            }
+            return value.Value<int>();
+        }
+
+        public float GetFloat(string key, float defaultValue = 0.0f)
+        {
+            if (_config == null)
+            {
+                return defaultValue;
+            }
+            var value = getValue(key);
+            if (value == null || value.Type != JTokenType.Float)
+            {
+                return defaultValue;
+            }
+            return value.Value<float>();
+        }
+
+        public bool GetBool(string key, bool defaultValue = false)
+        {
+            if (_config == null)
+            {
+                return defaultValue;
+            }
+            var value = getValue(key);
+            if (value == null || value.Type != JTokenType.Boolean)
+            {
+                return defaultValue;
+            }
+            return value.Value<bool>();
+        }
+    }
+}
